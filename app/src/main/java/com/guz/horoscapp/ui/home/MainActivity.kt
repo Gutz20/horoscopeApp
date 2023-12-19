c12d149811e4958b0e7b3134eff154ceedc356fe
Joe <darckProyect8@gmail.com>
2023-12-18 23:04:35 -0500

    initial commit

---
+++ app/src/main/java/com/guz/horoscapp/ui/home/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.guz.horoscapp.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.guz.horoscapp.R
+import com.guz.horoscapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initUI()
+    }
+
+    private fun initUI() {
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        val navHost = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHost.navController
+        binding.bottomNavView.setupWithNavController(navController)
+    }
+
+
+}
---
+++ app/src/main/java/com/guz/horoscapp/ui/luck/LuckFragment.kt
@@ -0,0 +1,27 @@
+package com.guz.horoscapp.ui.luck
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.guz.horoscapp.R
+import com.guz.horoscapp.databinding.FragmentHoroscopeBinding
+import com.guz.horoscapp.databinding.FragmentLuckBinding
+
+
+class LuckFragment : Fragment() {
+
+    private var _binding: FragmentLuckBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLuckBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+}
