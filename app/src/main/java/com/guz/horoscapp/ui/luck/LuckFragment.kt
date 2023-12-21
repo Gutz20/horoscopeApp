30da78ab770a0573ead74dd229727fdf0a52bde3
Joe <darckProyect8@gmail.com>
2023-12-20 20:25:55 -0500

    ViewModel

--- app/src/main/java/com/guz/horoscapp/ui/luck/LuckFragment.kt
+++ app/src/main/java/com/guz/horoscapp/ui/luck/LuckFragment.kt
@@ -8,8 +8,9 @@ import android.view.ViewGroup
 import com.guz.horoscapp.R
 import com.guz.horoscapp.databinding.FragmentHoroscopeBinding
 import com.guz.horoscapp.databinding.FragmentLuckBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class LuckFragment : Fragment() {
 
     private var _binding: FragmentLuckBinding? = null
